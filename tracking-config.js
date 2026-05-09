e72e6cd44b4741cba6cf4a4c9337f27222ca5316
Alessandro <info@MSI>
2026-05-09 21:17:20 +0200

    Set Google Ads conversion label

--- tracking-config.js
+++ tracking-config.js
@@ -2,7 +2,7 @@ window.TRACKING_CONFIG = {
     siteUrl: "https://promotions-studio-landing.quarkteam00.workers.dev",
     ga4MeasurementId: "",
     googleAdsId: "AW-18147097793",
-    googleAdsConversionLabel: "",
+    googleAdsConversionLabel: "Fj1rCN3etKocEMH5ms1D",
     gtmId: "",
     debug: false
 };
